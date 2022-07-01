e111a99c027bc8c37f03d4b5d74c62e6b75cc2fd
SaifAli143 <saifshaikh7274@gmail.com>
2022-07-01 17:01:14 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AUTHENTICATION_DEMO.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
